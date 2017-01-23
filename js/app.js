5f8216e2cf25858cb10654a22fcb09d509beaf92
mohdazmeer <mohd.azmeer.omar@gmail.com>
2017-01-23 15:08:26 +0800

    add tree view

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*global angular*/
-var profileApp = angular.module('profileApp', ['ngRoute', 'profileControllers']);
-profileApp.config(['$routeProvider', function ($routeProvider) {
+var profileApp = angular.module('profileApp', ['ngRoute', 'angular.filter', 'profileControllers']);
+profileApp.config(['$routeProvider', function($routeProvider) {
     'use strict';
     $routeProvider.when('/list', {
         templateUrl: 'template/list.html',
--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,15 @@
 /*global angular*/
-var profileControllers = angular.module('profileControllers', ['ngMap']);
-profileControllers.controller('ListController', function ($scope, $http) {
+var profileControllers = angular.module('profileControllers', ['ngMap', 'angular.filter']);
+profileControllers.controller('ListController', function($scope, $http) {
     'use strict';
-    $http.get('data/profiles.json').then(function (response) {
+    $http.get('data/profiles.json').then(function(response) {
         $scope.profiles = response.data;
+        $scope.nameOrder = "full_name";
     });
 });
-profileControllers.controller('DetailsController', function ($scope, $http, $routeParams) {
+profileControllers.controller('DetailsController', function($scope, $http, $routeParams) {
     'use strict';
-    $http.get('data/profiles.json').then(function (response) {
+    $http.get('data/profiles.json').then(function(response) {
         $scope.profiles = response.data;
         $scope.indexId = $routeParams.indexId;
     });
